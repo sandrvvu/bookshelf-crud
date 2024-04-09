fff7d7a485469236cd44d3bbe0529e2522a8279f
sandrvvu <sandrvvu@gmail.com>
2024-04-09 19:52:00 +0300

    editing wrong path navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import BookPage from "./pages/BookPage";
-import NoPage from "./pages/NoPage";
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/book/:id" element={<BookPage />} />
-        <Route path="*" element={<NoPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
